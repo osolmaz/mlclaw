eb743836c0cafdacce33cca83c05103f001192f9
Onur Solmaz <2453968+osolmaz@users.noreply.github.com>
2026-07-12 23:28:01 +0800

    build: pin framed-decision BrokerKit

--- src/mlclaw/runtime-image.ts
+++ src/mlclaw/runtime-image.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "node:url";
 
 const DEFAULT_OPENCLAW_VERSION = "2026.7.1-beta.5";
 const DEFAULT_BROKERKIT_PLUGIN_VERSION = "0.1.0";
-const DEFAULT_BROKERKIT_VERSION = "8b642525a8c7bcda28b95f9699c51ab3585e4dab";
+const DEFAULT_BROKERKIT_VERSION = "ec8e8833a35cec4342d19b79bb6b8c98e528530a";
 const DEFAULT_RUNTIME_IMAGE_REPOSITORY = "ghcr.io/osolmaz/mlclaw";
 
 const PACKAGE_METADATA = readPackageMetadata();
